e8bccbfaf133768d92685b91ff53b52eb9e9055e
ThitherShore <thither_shore@163.com>
2019-04-16 08:41:05 -0400

    .

--- scripts/randomization-cifar/run.py
+++ scripts/randomization-cifar/run.py
@@ -13,21 +13,31 @@ if __name__ == "__main__":
 	# sys.argv[1] train or eval: {train, eval}
     # sys.argv[2] proportion-random
     trn_or_ev = sys.argv[1] 
-    trn_or_ts = "train" if (trn_or_ev == "train") else "test"
     prop_rand = float(sys.argv[2])
     max_steps = sys.argv[3] 
 
-    for sp in np.arange(5, 10)/10:  # sparsity
-    	os.system("python3 prune_resnet_dns.py %s \
-            --dataset cifar10_%s.tfrecord \
-            --train-dir out-prune-%d-%d \
-            --warm-start out-%d \
-            --target-sparsity %s \
-            --proportion-random %s \
-            --max-steps %s" % (
-            	trn_or_ev, trn_or_ts,
-            	int(prop_rand*100), int(sp*10), int(prop_rand*100),
-            	str(sp), str(prop_rand), max_steps))
+    if trn_or_ev == "train":
+	    for sp in np.arange(5, 10)/10:  # sparsity
+	    	os.system("python3 prune_resnet_dns.py train \
+	            --dataset cifar10_train.tfrecord \
+	            --train-dir out-prune-%d-%d \
+	            --warm-start out-%d \
+	            --target-sparsity %s \
+	            --proportion-random %s \
+	            --max-steps %s" % (
+	            	int(prop_rand*100), int(sp*10), int(prop_rand*100),
+	            	str(sp), str(prop_rand), max_steps))
+	elif trn_or_ev == "eval":
+		for sp in np.arange(5, 10)/10:  # sparsity
+	    	os.system("python3 prune_resnet_dns.py eval \
+	            --dataset cifar10_test.tfrecord \
+	            --train-dir out-prune-%d-%d \
+	            --target-sparsity %s \
+	            --max-steps %s" % (
+	            	int(prop_rand*100), int(sp*10),
+	            	str(sp), str(prop_rand), max_steps))
+	else:
+		raise Exception("Please specify either train or eval!")
 
 
 
